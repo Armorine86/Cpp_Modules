58be6adcc6a2a4354967c104d4446d7140e5e7fc
mmondell <mmondell@c2r5p1.42quebec.com>
2021-12-03 09:16:02 -0500

    First Ex00

---
+++ Module00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 10:57:39 by mmondell          #+#    #+#             */
+/*   Updated: 2021/12/03 09:09:26 by mmondell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int32_t i = 1; argv[i]; i++)
+		{
+			for (int32_t j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
