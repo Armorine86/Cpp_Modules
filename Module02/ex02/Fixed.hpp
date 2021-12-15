1c6f98c7e0d38134f4d123670e3867d9dbfd0f55
Maxime Mondello <mmondell@student.42quebec.com>
2021-12-15 10:30:32 -0500

    .

--- Module02/ex02/Fixed.hpp
+++ Module02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 11:45:56 by mmondell          #+#    #+#             */
-/*   Updated: 2021/12/15 08:45:01 by mmondell         ###   ########.fr       */
+/*   Updated: 2021/12/15 10:30:14 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ class Fixed
 {
 
 private:
-	int32_t value;			   //* fixed point number --> A number that can holds fractional bits.
+	int32_t value;			   //* fixed point number --> A number that can hold fractional bits.
 	static const int32_t bits; //* Number of fractional bits (after the ",")
 
 public:
