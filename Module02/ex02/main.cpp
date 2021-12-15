e1810ca5c052b1127805ccab9c0db59b1aaf4e13
Maxime Mondello <mmondell@student.42quebec.com>
2021-12-15 08:16:32 -0500

    .

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 11:46:05 by mmondell          #+#    #+#             */
-/*   Updated: 2021/12/14 14:40:44 by mmondell         ###   ########.fr       */
+/*   Updated: 2021/12/15 08:16:24 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,32 +70,32 @@ int main(void)
     std::cout << "c = " << c << '\n';
     std::cout << "min(b, c) = " << Fixed::min(b, c) << "\n\n";
 
-    std::cout << "9.87f + 4.5f = " << 9.87f + 4.5f << '\n';
+    std::cout << "4.3f + 7.78 = " << 4.3f + 7.78f << '\n';
     std::cout << "c + 4.5 = " << c + Fixed(4.5f) << "\n\n";
 
-    std::cout << "9.87f - 4.5f = " << 9.87f - 4.5f << '\n';
-    std::cout << "c - 4.5 = " << c - Fixed(4.5f) << "\n\n";
+    std::cout << "4.3f - 7.78f = " << 4.3f - 7.78f << '\n';
+    std::cout << "c - 4.5 = " << c - Fixed(7.78f) << "\n\n";
 
-    std::cout << "9.87f * 4.5f = " << 9.87f * 4.5f << '\n';
-    std::cout << "c * 4.5 = " << c * Fixed(4.5f) << "\n\n";
+    std::cout << "4.3f * 7.78f = " << 4.3f * 7.78f << '\n';
+    std::cout << "c * 4.5 = " << c * Fixed(7.78f) << "\n\n";
 
-    std::cout << "9.87f / 4.5f = " << 9.87f / 4.5f << '\n';
+    std::cout << "4.3f / 7.78f = " << 4.3f / 7.78f << '\n';
     std::cout << "c / 4.5 = " << c / Fixed(4.5f) << "\n\n";
 
     std::cout << std::setw(7) << a << std::setw(4) << " > " << std::setw(7) << b << " = "
-              << std::boolalpha << (a > b) << '\n';
+              << std::boolalpha << (a > b) << std::endl;
     std::cout << std::setw(7) << c << std::setw(4) << " >= " << std::setw(7) << d << " = "
-              << std::boolalpha << (c >= d) << '\n';
+              << std::boolalpha << (c >= d) << std::endl;
     std::cout << std::setw(7) << a << std::setw(4) << " < " << std::setw(7) << b << " = "
-              << std::boolalpha << (a < b) << '\n';
+              << std::boolalpha << (a < b) << std::endl;
     std::cout << std::setw(7) << c << std::setw(4) << " <= " << std::setw(7) << d << " = "
-              << std::boolalpha << (c <= d) << '\n';
+              << std::boolalpha << (c <= d) << std::endl;
     std::cout << std::setw(7) << c << std::setw(4) << " <= " << std::setw(7) << b << " = "
-              << std::boolalpha << (c <= b) << '\n';
+              << std::boolalpha << (c <= b) << std::endl;
     std::cout << std::setw(7) << a << std::setw(4) << " == " << std::setw(7) << b << " = "
-              << std::boolalpha << (a == b) << '\n';
+              << std::boolalpha << (a == b) << std::endl;
     std::cout << std::setw(7) << c << std::setw(4) << " == " << std::setw(7) << d << " = "
-              << std::boolalpha << (c == d) << '\n';
+              << std::boolalpha << (c == d) << std::endl;
 	}
 	
 	
