94d7088e2b5987ed65fdf99d93ec73bc357d4838
Armorine86 <mmondell@student.42quebec.com>
2022-01-03 10:30:35 -0500

    Clang Formated

--- Module07/ex02/Array.hpp
+++ Module07/ex02/Array.hpp
@@ -12,72 +12,74 @@
 
 #pragma once
 
-#include <exception>
 #include <cstddef>
+#include <exception>
 
-template<class T>
-class Array {
-
+template <class T>
+class Array
+{
 public:
-	Array();
-	Array(unsigned int n);
-	Array(const Array &src);
-	Array& operator=(const T& rhs);
-	~Array() { delete[] array; }
-	T& operator[](unsigned int i);
+    Array();
+    Array(unsigned int n);
+    Array(const Array& src);
+    Array& operator=(const T& rhs);
+    ~Array() { delete[] array; }
+    T& operator[](unsigned int i);
 
-	unsigned int getSize();
-	
+    unsigned int getSize();
+
 private:
-	T *array;
-	unsigned int size;
+    T* array;
+    unsigned int size;
 };
 
-template<class T>
-Array<T>::Array() : array(NULL), size(0) {}
-
-template<class T>
-Array<T>::Array(unsigned int n) 
+template <class T>
+Array<T>::Array() : array(NULL), size(0)
 {
-	size = n;
-	array = new T[n]();
 }
 
-template<class T>
-Array<T>::Array(const Array &src) 
+template <class T>
+Array<T>::Array(unsigned int n)
 {
-	size = src.size;
-	
-	array = new T[size]();
-	for (unsigned int i = 0; i < size; i++)
-		array[i] = src.array[i];
+    size = n;
+    array = new T[n]();
 }
 
-template<typename T>
-Array<T>& Array<T>::operator=(const T &rhs)
+template <class T>
+Array<T>::Array(const Array& src)
 {
-	if (this == &rhs)
-		return *this;
-	
-	size = rhs.size;
-	
-	if (array != NULL)
-		delete[] array;
-	
-	for (int i = 0; i < size; i++)
-		array[i] = rhs.array[i];
+    size = src.size;
+
+    array = new T[size]();
+    for (unsigned int i = 0; i < size; i++)
+        array[i] = src.array[i];
 }
 
-template<class T>
-unsigned int Array<T>::getSize() 
+template <typename T>
+Array<T>& Array<T>::operator=(const T& rhs)
 {
-	return size;
+    if (this == &rhs)
+        return *this;
+
+    size = rhs.size;
+
+    if (array != NULL)
+        delete[] array;
+
+    for (int i = 0; i < size; i++)
+        array[i] = rhs.array[i];
 }
 
-template<class T>
+template <class T>
+unsigned int Array<T>::getSize()
+{
+    return size;
+}
+
+template <class T>
 T& Array<T>::operator[](unsigned int i)
 {
-	if (i >= size || i < 0)
-		throw std::exception();
-	return array[i];
+    if (i >= size || i < 0)
+        throw std::exception();
+    return array[i];
 }
