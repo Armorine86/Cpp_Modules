f05117f08a2d133a558d211750fe2b9659b32a2a
Armorine86 <mmondell@student.42quebec.com>
2022-01-04 14:16:52 -0500

    Expanded main

--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -6,40 +6,45 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:05:34 by mmondell          #+#    #+#             */
-/*   Updated: 2022/01/04 13:22:12 by mmondell         ###   ########.fr       */
+/*   Updated: 2022/01/04 14:16:39 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <vector>
+#include <list>
+#include <deque>
 #include "colors.hpp"
 #include "easyfind.hpp"
 
-void populate_vector(std::vector<int> &vec)
+template <typename T>
+void populate_container(T &cont)
 {
 	for (int i = 0; i < 25; i++)
-		vec.push_back(i + 1);
+		cont.push_back(i + 1);
 }
 
-void print_vector(std::vector<int> &vec)
+template <typename T>
+void print_vector(T &cont)
 {
 	std::vector<int>::iterator index;
 
 	std::cout << "Array: ";
 
 	// Using iterator, we don't need to keep track of the number of elements in the array
-	for(index = vec.begin(); index != vec.end(); index++)
+	for(index = cont.begin(); index != cont.end(); index++)
 		std::cout << *index << " ";							  			
 	std::cout << std::endl;
 }
 
 int main(void)
 {
-	std::vector<int> vec0;
+	std::vector<int> vec0; // https://en.cppreference.com/w/cpp/container/vector
 	std::vector<int>::iterator value; // Like a pointer, dereference to access value at its address.
 
-	populate_vector(vec0);
+	populate_container(vec0);
 	
+	std::cout << GREEN << "\nVECTORS" << END << std::endl;
 	print_vector(vec0);
 	
 	try {
@@ -55,4 +60,53 @@ int main(void)
 	} catch (std::exception &e) {
 		std::cout << RED << "Value not found" << END << std::endl;
 	}
+
+	std::cout << std::endl;
+
+
+
+	std::list<int> list1; // https://en.cppreference.com/w/cpp/container/list
+
+	std::cout << GREEN << "LINKED LIST" << END << std::endl;
+	populate_container(list1);
+
+	try {
+		value = easyfind(vec0, 15);
+		std::cout << "Value found: " << *value << std::endl;
+	} catch (std::exception &e) {
+		std::cout << RED << "Value not found" << END << std::endl;
+	}
+
+	try {
+		value = easyfind(vec0, 0);
+		std::cout << "Value found: " << *value << std::endl;
+	} catch (std::exception &e) {
+		std::cout << RED << "Value not found" << END << std::endl;
+	}
+	
+	std::cout << std::endl;
+
+
+
+	std::deque<int> deq1; // https://en.cppreference.com/w/cpp/container/deque
+
+	std::cout << GREEN << "DOUBLE-ENDED QUEUE" << END << std::endl;
+	populate_container(deq1);
+	
+	try {
+		value = easyfind(vec0, 23);
+		std::cout << "Value found: " << *value << std::endl;
+	} catch (std::exception &e) {
+		std::cout << RED << "Value not found" << END << std::endl;
+	}
+
+	try {
+		value = easyfind(vec0, 0);
+		std::cout << "Value found: " << *value << std::endl;
+	} catch (std::exception &e) {
+		std::cout << RED << "Value not found" << END << std::endl;
+	}
+
+	std::cout << std::endl;
+
 }
