13f5309c032526006d7ee73a97a9cc79728f49ba
Armorine86 <mmondell@student.42quebec.com>
2022-01-04 13:35:09 -0500

    added comments

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:01:51 by mmondell          #+#    #+#             */
-/*   Updated: 2022/01/04 13:26:02 by mmondell         ###   ########.fr       */
+/*   Updated: 2022/01/04 13:29:02 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typename T::iterator easyfind(T &arr, const int &target)
 {
 	typename T::iterator index;
 	
+	// Returns an iterator to first occurence of target
 	index = std::find(arr.begin(), arr.end(), target);
 	if (index == arr.end())
 		throw std::exception();
