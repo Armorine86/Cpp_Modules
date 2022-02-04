adec1654211d1374db8c46c31834208e673597b0
Armorine86 <mmondell@student.42quebec.com>
2022-02-04 13:51:11 -0500

    .

--- Module07/ex00/main.cpp
+++ Module07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmondell <mmondell@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 18:49:48 by mmondell          #+#    #+#             */
-/*   Updated: 2022/02/04 13:47:25 by mmondell         ###   ########.fr       */
+/*   Updated: 2022/02/04 13:51:02 by mmondell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,25 @@
 int main(void)
 {
     {    
-        int a = 1;
-        int b = 10;
+        int a = 2;
+        int b = 3;
         
-        std::cout << "a = " << a << "\nb = " << b << std::endl;
-        swap_T(a, b);
-        std::cout << "\na = " << a << "\nb = " << b << std::endl;
-        std::cout << "min( a, b ) = " << min_T(a, b) << std::endl;
-        std::cout << "max( a, b ) = " << max_T(a, b) << std::endl;
+        ::swap(a, b);
+        std::cout << "a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+        std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
     }
 
     std::cout << std::endl;
     
     {   
-        std::string c = "13";
-        std::string d = "12";
+        std::string c = "chaine1";
+        std::string d = "chaine2";
 
-        std::cout << "c = " << c << "\nd = " << d << std::endl;
-        swap_T(c, d);
-        std::cout << "\nc = " << c << "\nd = " << d << std::endl;
-        std::cout << "min( c, d ) = " << min_T(c, d) << std::endl;
-        std::cout << "max( c, d ) = " << max_T(c, d) << std::endl;
+        ::swap(c, d);
+        std::cout << "c = " << c << ", d = " << d << std::endl;
+        std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+        std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
     }
 
     std::cout << std::endl;
@@ -44,10 +42,9 @@ int main(void)
         float a = 4.2f;
         float b = 8.7f;
         
-        std::cout << "a = " << a << "\nb = " << b << std::endl;
-        swap_T(a, b);
-        std::cout << "\na = " << a << "\nb = " << b << std::endl;
-        std::cout << "min( a, b ) = " << min_T(a, b) << std::endl;
-        std::cout << "max( a, b ) = " << max_T(a, b) << std::endl;
+        ::swap(a, b);
+        std::cout << "a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+        std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
     }
 }
